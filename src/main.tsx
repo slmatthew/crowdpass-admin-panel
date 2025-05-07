0a51e3e1a2dbe73a906b114b4ed7a9cb9dc0d65c
slmatthew <36668268+slmatthew@users.noreply.github.com>
2025-05-08 01:00:46 +0300

    Refactor routing structure to include ModalProvider and improve component organization

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-import { ModalProvider } from "./context/ModalContext";
 
 const queryClient = new QueryClient();
 
@@ -13,10 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <ModalProvider>
-          <AppRouter />
-          <Toaster />
-        </ModalProvider>
+        <AppRouter />
+        <Toaster />
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
